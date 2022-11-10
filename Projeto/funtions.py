5f2f925512fe8e3d60abe82b6c7b47a63a17dbea
Dong-Xuyong <xuyongdong0@gmail.com>
2022-11-10 15:00:49 +0000

    first

---
+++ Projeto/funtions.py
@@ -0,0 +1,66 @@
+import os
+import pandas as pd #Library to handle with dataframes
+import matplotlib.pyplot as plt # Library to plot graphics
+import numpy as np # To handle with matrices
+import seaborn as sns # to build modern graphics
+from scipy.stats import kurtosis, skew # it's to explore some statistics of numerical values
+from scipy import stats
+import os
+
+
+
+PROJECT_ROOT_DIR = "."
+IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images")
+os.makedirs(IMAGES_PATH, exist_ok=True)
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+    
+def resumetable(df):
+    print(f"Dataset Shape: {df.shape}")
+    summary = pd.DataFrame(df.dtypes,columns=['dtypes'])
+    summary = summary.reset_index()
+    summary['Name'] = summary['index']
+    summary = summary[['Name','dtypes']]
+    summary['Missing'] = df.isnull().sum().values    
+    summary['Uniques'] = df.nunique().values
+    summary['First Value'] = df.loc[0].values
+    summary['Second Value'] = df.loc[1].values
+    summary['Third Value'] = df.loc[2].values
+
+    for name in summary['Name'].value_counts().index:
+        summary.loc[summary['Name'] == name, 'Entropy'] = round(stats.entropy(df[name].value_counts(normalize=True), base=2),2) 
+
+    return summary
+
+def CalcOutliers(df_num): 
+    
+    data_mean, data_std = np.mean(df_num), np.std(df_num)
+
+    # seting the cut line to both higher and lower values
+    # You can change this value
+    cut = data_std * 3
+
+    #Calculating the higher and lower cut values
+    lower, upper = data_mean - cut, data_mean + cut
+
+    # creating an array of lower, higher and total outlier values 
+    outliers_lower = [x for x in df_num if x < lower]
+    outliers_higher = [x for x in df_num if x > upper]
+    outliers_total = [x for x in df_num if x < lower or x > upper]
+
+    # array without outlier values
+    outliers_removed = [x for x in df_num if x > lower and x < upper]
+    
+    print('Identified lowest outliers: %d' % len(outliers_lower)) # printing total number of values in lower cut of outliers
+    print('Identified upper outliers: %d' % len(outliers_higher)) # printing total number of values in higher cut of outliers
+    print('Identified outliers: %d' % len(outliers_total)) # printing total number of values outliers of both sides
+    print('Non-outlier observations: %d' % len(outliers_removed)) # printing total number of non outlier values
+    print("Total percentual of Outliers: ", round((len(outliers_total) / len(outliers_removed) )*100, 4)) # Percentual of outliers in points
+    
+    return
+
